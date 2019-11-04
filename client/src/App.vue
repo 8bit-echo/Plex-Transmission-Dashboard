09d250960c5a5d8db1807a38fb113b1ee993dc32
Matt Dickey <matt.dickey@fourwindsinteractive.com>
2019-11-04 15:25:22 -0700

    fix status bar for iPhone X

--- client/src/App.vue
+++ client/src/App.vue
@@ -19,20 +19,3 @@
     components: { Nav, Notification, StatusBar, Modal }
   };
 </script>
-
-<style lang="scss">
-  #app {
-    font-family: 'Avenir', Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    width: 100vw;
-    height: 100%;
-    overflow-y: scroll;
-    -webkit-overflow-scrolling: touch; /* enables “momentum” (smooth) scrolling */
-    background: #3b3b48;
-  }
-  main {
-    padding-top: 32px;
-  }
-</style>
--- client/src/components/StatusBar.vue
+++ client/src/components/StatusBar.vue
@@ -1,37 +1,54 @@
 <template>
-    <div class="status-bar">
-      <div class="vpnStatus indicator">
-        VPN
-        <div
-          :class="{ statusLight: true, active: vpnStatus === 'ACTIVE' }"
-        ></div>
-      </div>
-      <div class="loading-indicator indicator" v-if="isLoading">
-        <img src="../assets/spinner.svg" width="25" />
-      </div>
+  <div
+    class="status-bar"
+    ref="statusBar"
+  >
+    <div class="vpnStatus indicator">
+      VPN
+      <div :class="{ statusLight: true, active: vpnStatus === 'ACTIVE' }"></div>
     </div>
+    <div
+      class="loading-indicator indicator"
+      v-if="isLoading"
+    >
+      <img
+        src="../assets/spinner.svg"
+        width="25"
+      />
+    </div>
+  </div>
 </template>
 
 <script>
-export default {
+  import { deviceType } from '@/functions.js';
+  export default {
     data() {
-    return {
-      vpnStatus: '-',
-      isLoading: false
-    };
-  },
-}
+      return {
+        vpnStatus: '-',
+        isLoading: false
+      };
+    },
+
+    mounted() {
+      if (deviceType() === 'iPhone X') {
+        this.$refs.statusBar.style.paddingTop = '45px';
+        this.$refs.statusBar.style.top = '0';
+      }
+    }
+  };
 </script>
 
 <style lang="scss" scoped>
   .status-bar {
     position: fixed;
-    // top: 0;
+    z-index: 3;
     background: rgb(32, 32, 39);
     width: 100%;
     display: flex;
     justify-content: space-between;
     align-items: center;
+    // padding-top: 45px;
+    // top: 0;
     .indicator {
       display: flex;
       align-items: center;
